e76029ca22b3d7cb533da04de43c23ceebe25a7f
Tonguesten36 <tung@tungsoft.net>
2025-02-05 13:43:46 +0700

    refactor: move sidebar component to crudview

--- src/components/crud/CrudView.vue
+++ src/components/crud/CrudView.vue
@@ -1,16 +1,20 @@
 <script setup>
 
 import ListComponent from "@/components/crud/UserListComponent.vue";
-import SidebarComponent from "@/components/crud/SidebarComponent.vue";
 </script>
 
 <template>
   <b-container fluid class="d-flex">
     <b-container class="crud-sidebar">
-      <SidebarComponent/>
+      <b-container class="d-grid gap-5">
+        <b-button squared class="mx-lg-0" size="lg" style="text-align: left; ">Users</b-button>
+        <b-button squared class="mx-lg-0" size="lg" style="text-align: left; ">Push Device</b-button>
+      </b-container>
     </b-container>
     <b-container class="crud-list">
-      <ListComponent/>
+      <component >
+        <ListComponent/>
+      </component>
     </b-container>
   </b-container>
 </template>
@@ -21,7 +25,11 @@ import SidebarComponent from "@/components/crud/SidebarComponent.vue";
   height: 100%;
   padding: 0;
 }
-
+.d-grid{
+  padding-top: 5rem;
+  display: flex;
+  justify-content: left;
+}
 .crud-list{
   width: 80%;
   /*border: 2px solid blue; /**/
--- src/components/crud/UserListComponent.vue
+++ src/components/crud/UserListComponent.vue
@@ -2,13 +2,13 @@
 <template>
   <b-container fluid class="crud-list-header">
     <h1>Users</h1>
-    <b-button squared class="mx-1 align-content-end align-self-center" size="lg">Add User</b-button>
+    <b-button squared class="mx-1 align-content-end align-self-center" size="lg" @click="addUser">Add User</b-button>
   </b-container>
   <b-container fluid class="crud-list-content">
     <b-table hover :items="dummyData" :fields="fields" responsive="sm" >
       <template #cell(actions)="row">
-        <b-button class="mx-2" size="sm">Edit</b-button>
-        <b-button class="mx-2" size="sm">Delete</b-button>
+        <b-button class="mx-2" size="sm" @click="editUser">Edit</b-button>
+        <b-button class="mx-2" size="sm" @click="deleteUser">Delete</b-button>
       </template>
     </b-table>
   </b-container>
@@ -51,6 +51,21 @@
     {id: 3, username: "cassava", email: "cassava@example.com"},
     {id: 0, username: "admin", email: "admin@example.com"},
   ]
+
+  function addUser(){
+    const newUser: User = {id: 90, username: "ntn", email: "ntn@example.com"};
+    dummyData.push(newUser);
+
+    console.log("new user added");
+  }
+  function editUser(){
+    //TODO: edit user when button clicked
+    console.log("edited user");
+  }
+  function deleteUser(){
+    //TODO: delete user when button clicked
+    console.log("deleted user");
+  }
 </script>
 
 
